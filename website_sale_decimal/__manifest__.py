ba035d014aeda19d5c10be95580a818f7cf8dc76
zahid <j.zahid@gmail.com>
2020-07-10 17:21:44 +0100

    Add files via upload

---
+++ website_sale_decimal/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Aurium technologies.
+{
+    'name': "Decimales in website sale cart quantities",
+    'version': '13.0.1.0.0',
+    'author': "Aurium Technologies",
+    'license': 'AGPL-3',
+    "depends": [
+        'website_sale',
+    ],
+    "data": [
+        'views/views.xml',
+        'views/website_sale_cart_lines.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'license':'AGPL-3',
+    'price':'12.0',
+    'currency':'EUR',
+}
